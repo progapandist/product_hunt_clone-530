7e90203947b255e859f194f93fd1c107fa24bea1
Andy B <andybarnov@gmail.com>
2021-03-05 10:21:29 +0100

    add my first workflow

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "first_name returns first name" do
+    user = User.new(first_name: "Andy")
+    assert_equal "Andy", user.first_name
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,5 +4,27 @@ class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
     assert_selector "h1", text: "Awesome Products" # if it's true the test PASSED
+    assert_selector ".card-product", count: Product.count
+  end
+
+  test "displays as much cards on index as Products in db" do
+    visit root_url
+    assert_selector ".card-product", count: Product.count
+  end
+
+  test "lets a signed in user create a new product" do
+    login_as users(:george) # Warden::Test::Helpers
+    visit "/products/new"
+    # save_and_open_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+
+    click_on "Create Product"
+    # save_and_open_screenshot
+
+    # Should be redirected to Home with new product
+    assert_equal root_path, page.current_path
+    assert_text "Change your life: Learn to code"
   end
 end
